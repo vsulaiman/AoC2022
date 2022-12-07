fa9678645b4c8d064a2788a1635598a97174cfe6
Vanessa Sulaiman <vanessa.sulaiman@tu-dortmund.de>
2022-12-07 11:44:18 +0100

    Day07

--- Day07/solution.cpp
+++ Day07/solution.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
             if (line.substr(2, found-2) == "cd") {
                 if (line.substr(found + 1) == "..") {
                     current_path.pop_back();
-                    current_path = current_path.substr(0, current_path.find_last_of('/'));
+                    current_path = current_path.substr(0, current_path.find_last_of('/')+1);
                     if (current_path.empty()) {
                         current_path = "/";
                     }
@@ -28,28 +28,19 @@ int main(int argc, char** argv) {
                 else {
                     current_path += line.substr(found + 1) + "/";
                 }
-                // std::cout << "now in: "<< current_path << std::endl;
-            }
-            else {
-                // ls
-                continue;
             }
         }
         else if(isdigit(line[0])) {
             std::string::size_type sz;
             int size = std::stoi(line, &sz);
             file_sizes[current_path + line.substr(sz + 1)] = size;
-            // std::cout << "file: " << line.substr(sz + 1) << " size: " << size << std::endl;
         }
-        // else {
-        //     //std::cout << "dir: " << line.substr(4) << std::endl;
-        // }
     }
 
     // sum data
     std::map<std::string, int> folder_sizes{{"/", 0}};
     for (const auto &[key, value] : file_sizes) {
-        std::cout << '[' << key << "] = " << value << std::endl;
+        // std::cout << '[' << key << "] = " << value << std::endl;
         folder_sizes["/"] += value;
         current_path = key.substr(0, key.find_last_of('/') + 1);
         while (current_path.size() > 1) {
@@ -63,14 +54,20 @@ int main(int argc, char** argv) {
             current_path = current_path.substr(0, current_path.find_last_of('/') + 1);
         }
     }
-    std::cout << "---" << std::endl;
+    int needed_space = folder_sizes["/"]-40000000;
+    int smallest = folder_sizes["/"];
     int sum = 0;
     for (const auto &[key, value] : folder_sizes) {
-        if (value < 100000)
-        {
-            std::cout << '[' << key << "] = " << value << std::endl;
+        // std::cout << '[' << key << "] = " << value << std::endl;
+        if (value <= 100000)
+        {            
             sum += value;
+        }
+        if (value >= needed_space && value < smallest) {
+            smallest = value;
         }
     }
     std::cout << sum << std::endl;
+
+    std::cout << smallest << std::endl;
 }
